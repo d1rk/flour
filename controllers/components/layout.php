ae76ea6af2d0c1c895790327feaec3feac3de016
Dirk Brünsicke <d1rk@gmx.de>
2010-03-11 10:25:40 +0100

    added layout-logic

--- controllers/components/layout.php
+++ controllers/components/layout.php
@@ -1,8 +1,6 @@
 <?php
-
 class LayoutComponent extends Object
 {
-
 	public $settings = array(
 	);
 
@@ -13,7 +11,15 @@ class LayoutComponent extends Object
 		$this->__controller = $controller;
 	}
 
-
+	function setup()
+	{
+		debug($this->params);
+		$this->__controller->theme = Configure::read('App.theme');
+		if (!empty($this->__controller->params['prefix']) && in_array($this->__controller->params['prefix'], Configure::read('Routing.prefixes')))
+		{
+			$this->__controller->layout = $this->__controller->params['prefix']; //'admin'; //set admin-layout for admin-routes.
+		}
+	}
 }
 
 ?>
