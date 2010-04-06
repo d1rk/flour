34c84676361424a8e152e2ff65e5a1c405b7a7ac
Dirk Brünsicke <d1rk@gmx.de>
2010-04-06 10:34:00 +0200

    added keywords and description to app.php, also added global topnav

--- templates/app/config/app.php
+++ templates/app/config/app.php
@@ -6,6 +6,8 @@ Configure::write('App.tagline', '');
 //layout / theme specific
 Configure::write('App.theme', 'minimal'); //default theme
 Configure::write('App.title', ':title - :name (:version)'); //used for title-tag
+Configure::write('App.keywords', '');
+Configure::write('App.description', '');
 Configure::write('App.breadcrumb', ' &raquo; '); //used as seperator for breadcrumbs
 
 ?>
