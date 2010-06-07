26ecf612f5626856ef48662203694453c58a1f1d
Dirk Brünsicke <d1rk@gmx.de>
2010-06-07 21:28:04 +0200

    updated contents controller with all needed actions

--- controllers/contents_controller.php
+++ controllers/contents_controller.php
@@ -2,40 +2,30 @@
 /**
  * ContentsController
  * 
- * Content is an object with unlimited amount of data.
- * It has a state and several meta-data that controls how and where
- * it is displayed.
- *
  * @package flour
  * @author Dirk Brünsicke
  * @version $Id$
- * @copyright bruensicke.com GmbH
+ * @copyright brünsicke.com GmbH
  **/
-class CustomListsController extends AppController
+class ContentsController extends AppController
 {
+
 	var $paginate = array(
-		'CustomList' => array(
+		'Content' => array(
 			'limit' => 100,
-			'recursive' => 1,
-		),
+		)
 	);
 
-
 /**
- * Index action.
+ * lists all available lotteries
  *
  * @access public
  */
 	function admin_index()
 	{
 		$conditions = array();
-		//$conditions = $this->_buildSearchConditions('CustomList', array(
-		//	'CustomList.name',
-		//));
-		$this->data = $this->paginate('CustomList', $conditions);
+		$this->data = $this->paginate('Content', $conditions);
 	}
-	
-
 
 /**
  * View action
@@ -43,36 +33,113 @@ class CustomListsController extends AppController
  * @access public
  * @param integer $id ID of record
  */	
-	function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid CustomList.', true));
-			$this->redirect(array('action'=>'index'));
+	function admin_view($id = null)
+	{
+		if (!$id)
+		{
+			return $this->Flash->error(
+				__('Invalid Content.', true),
+				$this->referer(array('action' => 'index'))
+			);
 		}
-		$this->set('data', $this->CustomList->read(null, $id));
+		$this->data = $this->Content->read(null, $id);
 	}
 
 /**
- * Called before the controller action.
+ * add action
  *
  * @access public
- */
-	function beforeFilter() {
+ */	
+	function admin_add()
+	{
+		if(!empty($this->data))
+		{
+			
+			if($this->Content->validates($this->data))
+			{
+				if($this->Content->save($this->data, false))
+				{
+					$id = $this->Content->getInsertId();
+					$this->Flash->success(
+						__('Content :Content.name saved.', true),
+						array('action' => 'view', $id)
+					);
+				} else {
+					return $this->Flash->error(
+						__('Content :Content.name could not be saved.', true)
+					);
+				}
+			}
+		}
 	}
 
 /**
- * Called after the controller action is run, but before the view is rendered.
+ * edit action
  *
  * @access public
- */
-	function beforeRender() {
+ * @param integer $id ID of record
+ */	
+	function admin_edit($id = null)
+	{
+		if (!$id)
+		{
+			return $this->Flash->error(
+				__('Invalid Content.', true),
+				$this->referer(array('action' => 'index'))
+			);
+		}
+		if(!empty($this->data))
+		{
+			
+			if($this->Content->validates($this->data))
+			{
+				if($this->Content->save($this->data, false))
+				{
+					$this->Flash->success(
+						__('Content :Content.name saved.', true),
+						array('action' => 'index')
+					);
+				} else {
+					return $this->Flash->error(
+						__('Content :Content.name could not be saved.', true)
+					);
+				}
+			}
+		}
+		$this->data = $this->Content->read(null, $id);
 	}
 
 /**
- * Called after the controller action is run and rendered.
+ * View action
  *
  * @access public
- */
-	function afterFilter() {
+ * @param integer $id ID of record
+ */	
+	function admin_delete($id = null)
+	{
+		if (!$id)
+		{
+			$this->Flash->error(
+				__('Invalid Content.', true),
+				$this->referer(array('action' => 'index'))
+			);
+		}
+		$this->data = $this->Content->read(null, $id);
+		$result = $this->Content->delete($id);
+		if(!$result)
+		{
+			return $this->Flash->error(
+				__('Content :Content.name could not be deleted.', true),
+				$this->referer(array('action'=>'index'))
+			);
+		}
+		$this->Flash->success(
+			__('Content :Content.name successfully deleted.', true),
+			$this->referer(array('action'=>'index'))
+		);
 	}
+
+
 }
+
 ?>
