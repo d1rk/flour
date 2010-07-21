d078495e3aea0b7f916842d032349bf13bca71c8
Armin Dressler <armin.dressler@bruensicke.com>
2010-07-21 08:48:42 +0200

    added to flour app model: - soft delete, undelete - find active, current, editions, deleted - add created_by and modified_by and deleted_by

--- flour_app_model.php
+++ flour_app_model.php
@@ -14,27 +14,59 @@ class FlourAppModel extends AppModel
 	/**
 	 * flour beforeSave
 	 *
-	 * @var array
-	 * @access private
+	 * @access public
 	 */
 	function beforeSave()
-	{
+	{		
 		$this->_addUserdata();
-
+		return true;
 	}
-
+	
 	/**
-	 * flour afterSave
+	 * flour soft delete
 	 *
-	 * @var array
-	 * @access private
+	 * @access public
 	 */
-	function afterSave()
-	{
+	function delete($id = null, $cascade = true) {
+		if (!empty($id)) {
+			$this->id = $id;
+		}
+		$id = $this->id;
+
+		if(!$this->hasField('deleted')) {
+			return parent::delete($id, $cascade);
+		}
 		
-
+		$user = $this->_getUser();
+		if($user) {
+			$this->data[$this->alias]['deleted_by'] = $user;
+		}
+		$this->data[$this->alias]['deleted'] = date('Y-m-d H:i:s');
+		
+		// TODO: handle cascade delete / soft delete
+		
+		return $this->save($this->data);
 	}
-
+	
+	/**
+	 * flour undelete
+	 *
+	 * @access public
+	 */
+	function undelete($id) {
+		$this->id = $id;
+		if(!$this->hasField('deleted') || !$this->exists()){
+			return false;
+		}
+		$this->data = $this->read(null, $id);
+		$this->_addUserData();
+		$this->data[$this->alias]['deleted'] = null;
+		$this->data[$this->alias]['deleted_by'] = null;
+		
+		return $this->save($this->data);
+	}
+	
+	
 	/**
 	 * flour find
 	 *
@@ -43,48 +75,152 @@ class FlourAppModel extends AppModel
 	 */
 	function find($type, $options = array())
 	{
+		if (!isset($options['conditions']) ) {
+			$options['conditions'] = array();
+		} 
+		
+		if (!isset($options['contain']) ) {
+			// This kills all non-explicit contains like hasMany etc
+			// $options['contain'] = array();
+		} 
+		
+		if (!isset($options['order']) ) {
+			$options['order'] = array();
+		}
+		
+		if($this->hasField('deleted') && ! isset($options['conditions'][$this->alias.'.'.$this->primaryKey])) {
+			$options['conditions'][$this->alias.'.deleted'] = null;
+		}
+		
 		switch ($type) {
 			case 'active':
-				if (!isset($options['conditions']) ) {
-					$options['conditions'] = array();
-				} 
-
-				if (!isset($options['contain']) ) {
-					$options['contain'] = array();
-				} 
-
-				if (!isset($options['order']) ) {
-					$options['order'] = array('Lottery.ending ASC');
-				} 
-
-				$options['conditions'] = array_merge(
-					$options['conditions'],
-					array(
-						$this->alias.'.status' => 1,
-						$this->alias.'.valid_from <=' => date('Y-m-d H:i:s'),
-						$this->alias.'.valid_to >=' => date('Y-m-d H:i:s'),
-					)
-				);
-				
-				$type = (  isset($options['conditions']['Lottery.id']) 
-						|| isset($options['conditions']['Lottery.slug']))
-					? 'first'
-					: 'all';
-
-			break;
+				$this->_setActive($options, $type);
+				break;
+			case 'current':
+				$this->_setCurrent($options, $type);
+				break;
+			case 'editions':
+				$this->_setEditions($options, $type);
+				break;
+			case 'deleted':
+				$this->_setDeleted($options, $type);
+				break;
 		}
 		return parent::find($type, $options);
 	}
-
-	function beforeFind($queryData) {
-		// debug($queryData);
-		return true;
-	}
-
-	function _addUserData(&$this->data)
+	
+	function _setActive(&$options, &$type)
 	{
-//		$this->data
+		$this->_setValid($options, $type);
+		$options['conditions'] = array_merge(
+			$options['conditions'],
+			array(
+				$this->alias.'.status' => 1,
+			)
+		);
+		$type = (isset($options['conditions'][$this->alias.'.'.$this->primaryKey]))
+			? 'first'
+			: 'all';
+	}
+	
+	function _setCurrent(&$options, &$type) {
+		$this->_setValid($options, $type);
+		$options['conditions'] = array_merge(
+			$options['conditions'],
+			array(
+				$this->alias.'.status' => 1,
+			)
+		);
+		$options['order'] = array_merge(
+			$options['order'],
+			array(
+				$this->alias.'.valid_from' => 'DESC',
+			)
+		);
+		$type = 'first';
+	}
+	
+	function _setEditions(&$options, &$type) {
+		$slug = null;
+		
+		// find all editions with same slug by reference id:
+		if(isset($options['conditions'][$this->alias.'.'.$this->primaryKey])) {
+			$slug = $this->field('slug', $options['conditions']);
+		}
+		
+		// find all editions with given slug
+		if(isset($options['conditions'][$this->alias.'.slug'])) {
+			$slug = $options['conditions'][$this->alias.'.slug'];
+		}
+
+		unset($options['conditions'][$this->alias.'.'.$this->primaryKey]);
+		$options['conditions'] = array_merge(
+			$options['conditions'],
+			array(
+				$this->alias.'.slug' => $slug,
+			)
+		);
+		
+		$type = 'all';
+	}
+	
+	function _setDeleted(&$options, &$type) {
+		unset($options['conditions'][$this->alias.'.deleted']);
+		
+		$options['conditions'] = array_merge(
+			$options['conditions'],
+			array( 'NOT' => array(
+					$this->alias.'.deleted' => NULL
+				),
+			)
+		);
+		$type = 'all';
+	}
+	
+	function _setValid(&$options, &$type) {
+		$options['conditions'] = array_merge(
+			$options['conditions'],
+			array(
+				'AND' => array(
+					array(
+						'OR' => array(
+							$this->alias.'.valid_from <=' => date('Y-m-d H:i:s'),
+							$this->alias.'.valid_from' => null,
+						)
+					),
+					array(
+						'OR' => array(
+							$this->alias.'.valid_to >=' => date('Y-m-d H:i:s'),
+							$this->alias.'.valid_to' => null,
+						),
+					),
+				),
+			)
+		);
 	}
 
+	function _addUserData()
+	{
+		$user = $this->_getUser();
+		if(!$user) return false;
+		
+		if(!isset($this->data[$this->alias][$this->primaryKey])) {
+			$this->data[$this->alias]['created_by'] = $user;
+		}
+		
+		$this->data[$this->alias]['modified_by'] = $user;
+	}
+
+	function _getUser()
+	{
+		$user = Configure::read('App.User.id');
+		if(!empty($user)) return $user;
+		
+		$user = Configure::read('Auth.User.id');
+		if(!empty($user)) return $user;
+		
+		return false;
+	}
+	
 }
 ?>
--- views/elements/contents/form_basic.ctp
+++ views/elements/contents/form_basic.ctp
@@ -1,7 +1,4 @@
 <?php
 echo $this->Form->input('Content.title');
 echo $this->Form->input('Content.body');
-
-echo $this->Form->input('Content.sidebar', array('type' => 'textarea'));
-
 ?>
